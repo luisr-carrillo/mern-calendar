8b6fa966207169d40113f1286c8a61e2533cc4a1
Luis R. Carrillo <luisr_carrillo@outlook.com>
2021-10-30 00:38:40 -0500

    ➕: Add FontAwesome dependency

--- apps/calendar/src/main.tsx
+++ apps/calendar/src/main.tsx
@@ -1,8 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { App } from './app/app';
+import App from './app/app';
+import './styles.css';
 
 ReactDOM.render(
   <StrictMode>
